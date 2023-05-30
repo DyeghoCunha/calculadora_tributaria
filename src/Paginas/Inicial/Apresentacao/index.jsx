bfc5041362303f0eec9527cde72cabb8136fa138
Dyegho <dyeghocunha@gmail.com>
2023-05-29 21:32:01 -0300

    (docs) Alteração nas informações da pagina

--- src/Paginas/Inicial/Apresentacao/index.jsx
+++ src/Paginas/Inicial/Apresentacao/index.jsx
@@ -9,7 +9,7 @@ export default function PaginaApresentacao() {
       <section className={styles.secaoContainer}>
         <h2 className={styles.subtitulo}>Introdução</h2>
 
-        <p className={styles.paragrafo}>A recuperação de créditos tributários é uma alternativa legal disponível para diversos setores, incluindo as clínicas médicas. Neste artigo, abordaremos a recuperação tributária específica para clínicas médicas, explorando as vantagens proporcionadas pelo regime tributário do Lucro Presumido. Através de uma abordagem jurídica, apresentaremos informações relevantes sobre o tema.</p>
+        <p className={styles.paragrafo}>A recuperação de créditos tributários é uma alternativa legal disponível para diversos setores, incluindo as clínicas médicas. Neste artigo, abordaremos a recuperação tributária específica para clínicas médicas, explorando as vantagens proporcionadas para o regime tributário do Lucro Presumido que reduz a presunção do <strong className={styles.destaque}>IRPJ de 32% para 8% e da Csll de 32% para 12% garantindo uma restituição de até 70% do valor pago. </strong></p>
       </section>
 
       <section className={styles.secaoContainer}>
@@ -17,7 +17,11 @@ export default function PaginaApresentacao() {
 
         <p className={styles.paragrafo}>As clínicas médicas desempenham um papel crucial na sociedade, proporcionando serviços essenciais relacionados à saúde. No entanto, além dessa importante função, as clínicas enfrentam diversas outras preocupações, como a gestão de funcionários, insumos, medicamentos, equipamentos e, naturalmente, a questão dos impostos.
 
+          <br /> <br />
+
           Diante de tantas responsabilidades, é comum que a atenção para essa última questão seja minimizada. Muitos proprietários de clínicas simplesmente confiam que os valores devidos correspondem aos montantes legalmente estabelecidos em suas obrigações fiscais.
+
+          <br /> <br />
 
           Surpreendentemente, após uma análise cuidadosa, pode-se descobrir que nem todas as obrigações tributárias são legítimas, o que abre a possibilidade de recuperação dos créditos pagos em excesso pelas clínicas.</p>
       </section>
@@ -25,72 +29,92 @@ export default function PaginaApresentacao() {
       <section className={styles.secaoContainer}>
         <h2 className={styles.subtitulo}>Recuperação Tributária para clínicas médicas:</h2>
 
-        <p className={styles.paragrafo}>Desde a promulgação da Lei nº 9.249/1995, as clínicas, hospitais e laboratórios enquadrados no regime tributário do Lucro Presumido passaram a ser submetidos a uma apuração diferenciada do Imposto de Renda Pessoa Jurídica (IRPJ) e da Contribuição Social sobre o Lucro Líquido (CSLL).
+        <p className={styles.paragrafo}>Desde a promulgação da <strong className={styles.destaque}>Lei nº 9.249/1995</strong>, as clínicas, hospitais e laboratórios enquadrados no regime tributário do Lucro Presumido passaram a ser submetidos a uma apuração diferenciada do Imposto de Renda Pessoa Jurídica (IRPJ) e da Contribuição Social sobre o Lucro Líquido (CSLL).
 
-          Conforme as regras, as empresas do Lucro Presumido são obrigadas a recolher o IRPJ e a CSLL com base em uma presunção de 32% sobre a receita bruta. No entanto, quando se trata de serviços médicos relacionados à saúde, essa presunção sofre algumas alterações.
+          <br /> <br />
 
-          Para clínicas médicas enquadradas no Lucro Presumido, devido à redução da base de cálculo, a alíquota do IRPJ passa a ser de 8% (conforme o artigo 15, caput, da Lei 9.249), enquanto a alíquota da CSLL se torna 12% (conforme o artigo 20, inciso III, da Lei 9.249).
+          Conforme as regras, as empresas do Lucro Presumido são obrigadas a recolher o IRPJ e a CSLL com base em uma presunção de <strong className={styles.destaque}>32%</strong> sobre a receita bruta. No entanto, quando se trata de serviços médicos relacionados à saúde, essa presunção sofre algumas alterações.
 
-          Esses dois impostos compõem grande parte da carga tributária para empresas desse segmento. Recuperar esses valores pode gerar economias significativas para as clínicas que buscam a revisão tributária.
+          <br /> <br />
 
-          Como mencionado anteriormente, nem todas as clínicas médicas podem recuperar créditos tributários. No entanto, aquelas que estão enquadradas no Lucro Presumido e atendem a certas condições podem solicitar a revisão dos créditos tributários pagos nos últimos cinco anos (60 meses).
+          Para clínicas médicas enquadradas no Lucro Presumido, devido à redução da base de cálculo, a alíquota do IRPJ passa a ser de<strong className={styles.destaque}>8%</strong>(conforme o artigo 15, caput, da Lei 9.249), enquanto a alíquota da CSLL se torna <strong className={styles.destaque}>12%</strong> (conforme o artigo 20, inciso III, da Lei 9.249).
+
+          <br /> <br />
+
+          Esses dois impostos compõem grande parte da carga tributária para empresas desse segmento. Recuperar esses valores pode gerar <strong className={styles.destaque}>economias significativas</strong> para as clínicas que buscam a revisão tributária.
+
+          <br /> <br />
+
+          Como mencionado anteriormente, nem todas as clínicas médicas podem recuperar créditos tributários. No entanto, aquelas que estão enquadradas no Lucro Presumido e atendem a certas condições podem solicitar a revisão dos créditos tributários pagos nos últimos <strong className={styles.destaque}>cinco anos</strong> (60 meses).
         </p>
+        <br /> <br />
 
-
-        <h3> A seguir, apresentamos uma lista de algumas clínicas que podem recorrer à recuperação tributária: </h3>
-        <ul>
-          <li>Clínica de fonoaudiologia</li>
-          <li>Clínicas de cirurgias plásticas</li>
-          <li>Clínicas de fisioterapia</li>
-          <li>Clínicas de oftalmologia</li>
-          <li>Clínicas de ortopedia e traumatologia</li>
-          <li>Clínicas de procedimentos dermatológicos</li>
-          <li>Clínicas e laboratórios que realizam exames de imagem</li>
-          <li>Clínicas odontológicas</li>
-          <li>Clínicas pediátricas</li>
-          <li>Clínicas que realizam exames de diagnóstico</li>
-          <li>Empresas de home care</li>
+        <h3 className={styles.tituloLista}>Algumas clínicas que podem recorrer à recuperação tributária: </h3>
+        <ul className={styles.listaContainer}>
+          <li className={styles.listaItem}>Clínica de fonoaudiologia</li>
+          <li className={styles.listaItem}>Clínicas de cirurgias plásticas</li>
+          <li className={styles.listaItem}>Clínicas de fisioterapia</li>
+          <li className={styles.listaItem}>Clínicas de oftalmologia</li>
+          <li className={styles.listaItem}>Clínicas de ortopedia e traumatologia</li>
+          <li className={styles.listaItem}>Clínicas de procedimentos dermatológicos</li>
+          <li className={styles.listaItem}>Clínicas e laboratórios que realizam exames de imagem</li>
+          <li className={styles.listaItem}>Clínicas odontológicas</li>
+          <li className={styles.listaItem}>Clínicas pediátricas</li>
+          <li className={styles.listaItem}>Clínicas que realizam exames de diagnóstico</li>
+          <li className={styles.listaItem}>Empresas de home care</li>
         </ul>
 
         <p className={styles.paragrafo}> Além do enquadramento no Lucro Presumido, é importante ressaltar que a clínica deve ser uma sociedade empresária (não pode ser uma EIRELI ou sociedade simples) e deve possuir autorização de funcionamento confirmada pela ANVISA (Agência Nacional de Vigilância Sanitária).
 
+          <br /> <br />
+
           Cabe destacar que a Receita Federal não notifica os negócios sobre a possibilidade de recuperação de créditos tributários. Portanto, para descobrir se sua clínica médica tem direito à recuperação de créditos tributários, é fundamental contar com o auxílio de uma empresa especializada. </p>
-</section>
 
-        <section className={styles.secaoContainer}>
-          <h2 className={styles.subtitulo}>Benefícios da recuperação tributária para clínicas:</h2>
+      </section>
 
-          <p className={styles.paragrafo}>A recuperação de créditos tributários é um processo legal que pode ser realizado tanto administrativamente quanto judicialmente. Apesar de ser um procedimento relativamente simples, ainda é pouco conhecido entre os empresários, o que confere à clínica médica que solicita a recuperação dos créditos uma vantagem competitiva significativa em relação à concorrência.
-
-            Um acompanhamento tributário diligente pode revelar brechas legais para a redução da carga tributária, possibilitando que a empresa pague significativamente menos impostos do que inicialmente acreditava ser obrigada a pagar.
-
-            Após o levantamento de todos os pagamentos tributários dos últimos cinco anos, é possível identificar os tributos pagos indevidamente, solicitando a recuperação dos valores pagos a mais durante esse período de 60 meses.
-
-            A Tributo Justo, atuante no mercado de recuperação tributária há seis anos, já recuperou mais de R$1 bilhão em créditos tributários para centenas de empresas em todo o país. Para descobrir se sua clínica médica possui direito à recuperação de créditos tributários, clique no botão "Diagnóstico Tributário Gratuito" abaixo e receba, em até 48 horas, o contato de um de nossos consultores.
-          </p>
-        </section>
-
-
-        <section className={styles.secaoContainer}>
-          <h2 className={styles.subtitulo}>Descubra a Natureza e Procedimento do Crédito Tributário para Clínicas Médicas</h2>
-
-          <p className={styles.paragrafo}>No presente artigo, abordaremos o conceito e a aplicação do crédito tributário, destacando como as clínicas médicas podem se beneficiar legalmente desse recurso para aprimorar suas finanças. No Brasil, as obrigações tributárias são abrangentes e frequentemente representam um ônus considerável para as empresas. No setor da saúde, a tributação é particularmente complexa, uma realidade bem conhecida pelos empreendedores do ramo. No entanto, muitos desconhecem a possibilidade de obter créditos tributários, o que pode contribuir para a redução de custos e o aumento da rentabilidade das clínicas.
-
-            As clínicas estão sujeitas a diversas obrigações fiscais e tributárias, que podem resultar em custos significativos para o negócio. Neste artigo, ofereceremos orientações sobre como é possível reduzir esses encargos.</p>
-        </section>
-
-        <section className={styles.secaoContainer}>
-          <h2 className={styles.subtitulo}>Introdução</h2>
-
-          <p className={styles.paragrafo}>A recuperação de créditos tributários é uma alternativa legal disponível para diversos setores, incluindo as clínicas médicas. Neste artigo, abordaremos a recuperação tributária específica para clínicas médicas, explorando as vantagens proporcionadas pelo regime tributário do Lucro Presumido. Através de uma abordagem jurídica, apresentaremos informações relevantes sobre o tema.</p>
-        </section>
-
-
-     
       <section className={styles.secaoContainer}>
+
+        <h2 className={styles.subtitulo}>Benefícios da recuperação tributária para clínicas:</h2>
+
+        <p className={styles.paragrafo}>A recuperação de créditos tributários é um processo legal que pode ser realizado tanto administrativamente quanto judicialmente. Apesar de ser um procedimento relativamente simples, ainda é pouco conhecido entre os empresários, o que confere à clínica médica que solicita a recuperação dos créditos uma vantagem competitiva significativa em relação à concorrência.
+
+          <br /> <br />
+
+          Um acompanhamento tributário diligente pode revelar brechas legais para a redução da carga tributária, possibilitando que a empresa pague significativamente menos impostos do que inicialmente acreditava ser obrigada a pagar.
+
+          <br /> <br />
+
+          Após o levantamento de todos os pagamentos tributários dos últimos cinco anos, é possível identificar os tributos pagos indevidamente, solicitando a recuperação dos valores pagos a mais durante esse período de 60 meses.
+
+          <br /> <br />
+
+          A Tributo Justo, atuante no mercado de recuperação tributária há seis anos, já recuperou mais de R$1 bilhão em créditos tributários para centenas de empresas em todo o país. Para descobrir se sua clínica médica possui direito à recuperação de créditos tributários, clique no botão "Diagnóstico Tributário Gratuito" abaixo e receba, em até 48 horas, o contato de um de nossos consultores.
+        </p>
+      </section>
+
+
+      <section className={styles.secaoContainer}>
+
+        <h2 className={styles.subtitulo}>Descubra a Natureza e Procedimento do Crédito Tributário para Clínicas Médicas</h2>
+
+        <p className={styles.paragrafo}>No presente artigo, abordaremos o conceito e a aplicação do crédito tributário, destacando como as clínicas médicas podem se beneficiar legalmente desse recurso para aprimorar suas finanças. No Brasil, as obrigações tributárias são abrangentes e frequentemente representam um ônus considerável para as empresas. No setor da saúde, a tributação é particularmente complexa, uma realidade bem conhecida pelos empreendedores do ramo. No entanto, muitos desconhecem a possibilidade de obter créditos 
+        tributários, o que pode contribuir para a redução de custos e o aumento da rentabilidade das clínicas.
+
+        <br/> <br/>
+
+          As clínicas estão sujeitas a diversas obrigações fiscais e tributárias, que podem resultar em custos significativos para o negócio. Neste artigo, ofereceremos orientações sobre como é possível reduzir esses encargos.</p>
+
+      </section>
+
+
+
+      <section className={styles.secaoContainer}>
+
         <h2 className={styles.subtitulo}>Contato e Serviços da GroselhaCont</h2>
 
         <p className={styles.paragrafo}>A GroselhaCont oferece serviços especializados em recuperação de créditos tributários para clínicas médicas. Por meio de uma análise minuciosa da situação fiscal da clínica, é possível identificar os tributos pagos indevidamente e solicitar a recuperação dos valores pagos a mais. Isso pode gerar uma melhoria significativa na saúde financeira das clínicas médicas.</p>
+        
+        <br/><br/>
 
         <p className={styles.paragrafo}>Não deixe de entrar em contato com a GroselhaCont para obter mais informações sobre como sua clínica médica pode se beneficiar da recuperação de créditos tributários e reduzir seus custos. Aproveite a oportunidade de realizar um <a className={styles.linkBotao} href='/'>Diagnóstico Tributário Gratuito</a>para descobrir se sua clínica é elegível para esse processo.</p>
       </section>
